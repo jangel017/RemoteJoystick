4eed4fa4f9f4f4fd55fe67c353ebd6f91fd27e5d
jangel017 <142262468+jangel017@users.noreply.github.com>
2024-06-12 00:23:29 -0500

    Update test.py
    chaging port to 5000

--- test.py
+++ test.py
@@ -36,4 +36,4 @@ def emulate_joystick():
 threading.Thread(target=emulate_joystick, daemon=True).start()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)  # Escuchar en todas las interfaces de red
+    app.run(host='0.0.0.0', port=5000)  # Escuchar en todas las interfaces de red
