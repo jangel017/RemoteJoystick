42a9d90a8ec42a06fa25bf2b5aa27bb0d30b2471
jangel017 <142262468+jangel017@users.noreply.github.com>
2024-06-18 10:51:08 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,20 +1,42 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-import json
+import uinput
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app)
+
+# Crear un joystick virtual con python-uinput
+device = uinput.Device([
+    uinput.BTN_JOYSTICK,
+    uinput.ABS_X + (-100, 100, 0, 0),
+    uinput.ABS_Y + (-100, 100, 0, 0)
+])
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('simulate.html')
 
 @socketio.on('joystick_data')
-def handle_joystick_data(data):
-    print('Received joystick data:', data)  # Imprime los datos recibidos en la consola del servidor
-    with open('joystick_data.json', 'w') as f:
-        json.dump(data, f)
-    emit('response', {'status': 'received'})  # Responde al cliente
+def handle_joystick_data(json_data):
+    print('Received joystick data:', json_data)
+
+    x_value = 0
+    y_value = 0
+
+    if 'ArrowUp' in json_data['keys']:
+        y_value = -100
+    elif 'ArrowDown' in json_data['keys']:
+        y_value = 100
+    if 'ArrowLeft' in json_data['keys']:
+        x_value = -100
+    elif 'ArrowRight' in json_data['keys']:
+        x_value = 100
+
+    device.emit(uinput.ABS_X, x_value, syn=False)
+    device.emit(uinput.ABS_Y, y_value)
+    device.emit(uinput.BTN_JOYSTICK, 1)
+
+    emit('response', {'status': 'received'})
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
