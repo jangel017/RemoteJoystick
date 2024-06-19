7be71002f926cbb2831f2d1fcf2e20f83c9b046e
jangel017 <142262468+jangel017@users.noreply.github.com>
2024-06-18 20:09:59 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,18 +1,8 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
-from flask_cors import CORS
-import uinput
 
 app = Flask(__name__)
-CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-# Create the uinput device
-device = uinput.Device([
-    uinput.BTN_A,
-    uinput.ABS_X + (0, 255, 0, 0),
-    uinput.ABS_Y + (0, 255, 0, 0)
-])
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -20,25 +10,13 @@ def index():
 
 @socketio.on('joystick_data')
 def handle_joystick_data(json):
-    keys = json.get('keys')  # Obtener el valor de la clave 'key' o None si no existe
+    keys = json.get('keys')  # Obtener el valor de la clave 'keys' o None si no existe
     if keys is not None:
-        # Procesar el valor de 'key' si existe
+        # Procesar el valor de 'keys' si existe
         print("Valor de 'keys' recibido:", keys)
-
-        # Map the key to joystick buttons
-        event_map = {
-            'ArrowUp': uinput.ABS_Y,
-            'ArrowDown': uinput.ABS_Y,
-            'ArrowLeft': uinput.ABS_X,
-            'ArrowRight': uinput.ABS_X,
-            'a': uinput.BTN_A
-        }
-
-        if keys in event_map:
-            device.emit(event_map[keys], 1)  # Press
-            device.emit(event_map[keys], 0)  # Release
+        # Aquí puedes agregar cualquier lógica adicional que necesites
     else:
         print("La clave 'keys' no está presente en el diccionario recibido:", json)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5353, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
